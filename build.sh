bcfac8d01138a48273062d0fc68bb07ec4f39057
leftside97 <l3f7s1d3@protonmail.com>
2021-04-28 21:29:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,18 +6,19 @@ banner(){
 	echo
 	echo ""
 	echo -e "\t\tcoreboot-builder\n"
-	echo -e "\t0. git clone https://review.coreboot.org/coreboot "
-	echo -e "\t1. make crossgcc-i386 CPUS=$(nproc) "
-	echo -e "\t2. make -C payloads/coreinfo "
-	echo -e "\t3. make menuconfig "
-	echo -e "\t4. cat defconfig  "
-	echo -e "\t5. make "
-	echo -e "\t6. tutorial/part1 \n"
-	echo -e "\t7. qemu-system-x86_64 -bios build/coreboot.rom -serial stdio"
-	echo -e "\t8. qemu-system-x86_64 -bios build/coreboot.rom -cdrom <iso> "
+	echo -e "\t0. install tools"
+	echo -e "\t1. git clone https://review.coreboot.org/coreboot "
+	echo -e "\t2. make crossgcc-i386 CPUS=$(nproc) "
+	echo -e "\t3. make -C payloads/coreinfo "
+	echo -e "\t4. make menuconfig "
+	echo -e "\t5. cat defconfig  "
+	echo -e "\t6. make "
+	echo -e "\t7. tutorial/part1 \n"
+	echo -e "\t8. qemu-system-x86_64 -bios build/coreboot.rom -serial stdio"
+	echo -e "\t9. qemu-system-x86_64 -bios build/coreboot.rom -cdrom <iso> "
 
 }
-installstuff{
+install_stuff{
 	sudo dnf install git make gcc-gnat flex bison xz bzip2 gcc g++ ncurses-devel wget zlib-devel patch
 }
 
@@ -32,15 +33,16 @@ tutorial_part1(){
 }
 
 case $1 in
-	"0") git clone https://review.coreboot.org/coreboot;;
-	"1") cd coreboot && make crossgcc-i386 CPUS=$(nproc);;
-	"2") cd coreboot && make -C payloads/coreinfo;;
-	"3") cd coreboot && make menuconfig;;
-	"4") cd coreboot && make savedefconfig && cat defconfig;;
-	"5") cd coreboot && make;;
-	"6") tutorial_part1;;
-	"7") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom -serial stdio;;
-	"8") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom --cdrom $2;;
+	"0") install_stuff;;
+	"1") git clone https://review.coreboot.org/coreboot;;
+	"2") cd coreboot && make crossgcc-i386 CPUS=$(nproc);;
+	"3") cd coreboot && make -C payloads/coreinfo;;
+	"4") cd coreboot && make menuconfig;;
+	"5") cd coreboot && make savedefconfig && cat defconfig;;
+	"6") cd coreboot && make;;
+	"7") tutorial_part1;;
+	"8") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom -serial stdio;;
+	"9") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom --cdrom $2;;
 
 	*) banner;;
 esac

70e153cb8031e26e6f818151191e3f834af5c920
leftside97 <l3f7s1d3@protonmail.com>
2021-04-28 21:26:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,23 @@ banner(){
 	echo -e "\t3. make menuconfig "
 	echo -e "\t4. cat defconfig  "
 	echo -e "\t5. make "
-	echo -e "\t6. qemu-system-x86_64 -bios build/coreboot.rom -serial stdio"
-	echo -e "\t7. qemu-system-x86_64 -bios build/coreboot.rom -cdrom <iso> \n"
+	echo -e "\t6. tutorial/part1 \n"
+	echo -e "\t7. qemu-system-x86_64 -bios build/coreboot.rom -serial stdio"
+	echo -e "\t8. qemu-system-x86_64 -bios build/coreboot.rom -cdrom <iso> "
+
+}
+installstuff{
+	sudo dnf install git make gcc-gnat flex bison xz bzip2 gcc g++ ncurses-devel wget zlib-devel patch
+}
+
+tutorial_part1(){
+	git clone https://review.coreboot.org/coreboot
+	cd coreboot
+	make crossgcc-i386 CPUS=$(nproc)
+	make -C payloads/coreinfo
+	make menuconfig
+	make savedefconfig && cat defconfig
+	make
 }
 
 case $1 in
@@ -23,8 +38,10 @@ case $1 in
 	"3") cd coreboot && make menuconfig;;
 	"4") cd coreboot && make savedefconfig && cat defconfig;;
 	"5") cd coreboot && make;;
-	"6") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom -serial stdio;;
-	"7") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom --cdrom $2;;
+	"6") tutorial_part1;;
+	"7") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom -serial stdio;;
+	"8") cd coreboot && qemu-system-x86_64 -bios build/coreboot.rom --cdrom $2;;
+
 	*) banner;;
 esac
 
